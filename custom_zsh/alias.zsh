2525955965427068d711507e9561f2242e7a1f97
Colin MacRae <cmac4603@users.noreply.github.com>
2023-01-11 10:24:38 +0000

    alias

---
+++ custom_zsh/alias.zsh
@@ -0,0 +1,12 @@
+alias kc="kubectl"
+alias kx="kubectx"
+alias kn="kubens"
+
+alias vpn="osascript -e 'tell application \"Tunnelblick\"' -e '  connect \"vpn.hearstapps.com\"' -e 'end tell'"
+alias clear-dns="sudo killall -HUP mDNSResponder"
+alias whatismyip="dig TXT +short o-o.myaddr.l.google.com @ns1.google.com"
+
+alias nv="nvim"
+
+alias gwa="git worktree add"
+alias gwr="git worktree remove"
