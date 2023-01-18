649036df01f61b34d4757703aaf3f7104d7dae20
Colin MacRae <cmac4603@users.noreply.github.com>
2023-01-18 22:03:11 +0000

    bluloco and some tmux again with the mem thing

--- nvim/lua/cmac4603/colorscheme.lua
+++ nvim/lua/cmac4603/colorscheme.lua
@@ -1,4 +1,11 @@
-local colorscheme = "zephyr"
+local colorscheme = "bluloco"
+
+require("bluloco").setup({
+  style = "dark",
+  transparent = false,
+  italics = false,
+  terminal = vim.fn.has("gui_running") == 1 -- bluoco colors are enabled in gui terminals per default.
+})
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
