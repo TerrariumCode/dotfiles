4d499c69e4b4390ea98521f0b21cec669768fd1f
cmac4603 <cmac4306users.noreply.github.com>
2023-01-22 19:42:40 +0000

    back to zephyr

--- nvim/lua/cmac4603/colorscheme.lua
+++ nvim/lua/cmac4603/colorscheme.lua
@@ -1,11 +1,4 @@
-local colorscheme = "bluloco"
-
-require("bluloco").setup({
-  style = "dark",
-  transparent = false,
-  italics = false,
-  terminal = vim.fn.has("gui_running") == 1 -- bluoco colors are enabled in gui terminals per default.
-})
+local colorscheme = "zephyr"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
--- nvim/lua/cmac4603/plugins.lua
+++ nvim/lua/cmac4603/plugins.lua
@@ -113,10 +113,10 @@ return packer.startup(function(use)
 
   -- Colorschemes
   use { "glepnir/zephyr-nvim" }
-  use {
-    'uloco/bluloco.nvim',
-    requires = { 'rktjmp/lush.nvim' }
-  }
+  -- use {
+  --   'uloco/bluloco.nvim',
+  --   requires = { 'rktjmp/lush.nvim' }
+  -- }
   -- use { "rebelot/kanagawa.nvim" }
   -- use { "Rigellute/shades-of-purple.vim" }
   -- use { "folke/tokyonight.nvim" }
