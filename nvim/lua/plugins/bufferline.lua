118b41b24b5fb95ce74dc1b2a7d8dd8a02bd406f
Colin MacRae <cmac4603@users.noreply.github.com>
2023-03-14 09:24:45 +0000

    fixed barbar!!!!

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -2,6 +2,7 @@ return {
     {
         "romgrk/barbar.nvim",
         dependencies = "nvim-tree/nvim-web-devicons",
+        event = "BufEnter",
         opts = {
             auto_hide = false,
             hide = {extensions = false, inactive = false},
