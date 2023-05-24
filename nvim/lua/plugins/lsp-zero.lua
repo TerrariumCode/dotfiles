7077f4b4d2bf9e9f3be30fa8f8570bb5429309e1
Colin MacRae <cmac4603@users.noreply.github.com>
2023-05-24 22:04:37 +0100

    nearly there with lsp-zero v2

--- nvim/lua/plugins/lsp-zero.lua
+++ nvim/lua/plugins/lsp-zero.lua
@@ -1,30 +1,123 @@
 return {
-    -- LSP
-    {
-        "VonHeikemen/lsp-zero.nvim",
-        branch = "v2.x",
-        dependencies = {
-            -- LSP Support
-            {"neovim/nvim-lspconfig"},             -- Required
-            {                                      -- Optional
-              'williamboman/mason.nvim',
-              build = function()
-                pcall(vim.cmd, 'MasonUpdate')
-              end,
-            },
-            {"williamboman/mason-lspconfig.nvim"}, -- Optional
+  {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    lazy = true,
+    config = function()
+      -- This is where you modify the settings for lsp-zero
+      -- Note: autocompletion settings will not take effect
 
-            -- Autocompletion
-            {"hrsh7th/nvim-cmp"},         -- Required
-            {"hrsh7th/cmp-nvim-lsp"},     -- Required
-            {"hrsh7th/cmp-buffer"},       -- Optional
-            {"hrsh7th/cmp-path"},         -- Optional
-            {"saadparwaiz1/cmp_luasnip"}, -- Optional
-            {"hrsh7th/cmp-nvim-lua"},     -- Optional
+      require('lsp-zero.settings').preset({})
+    end
+  },
 
-            -- Snippets
-            {"L3MON4D3/LuaSnip"},             -- Required
-            {"rafamadriz/friendly-snippets"}, -- Optional
-        }
+  -- Autocompletion
+  {
+    'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
+    dependencies = {
+      {'L3MON4D3/LuaSnip'},
     },
+    config = function()
+      -- Here is where you configure the autocompletion settings.
+      -- The arguments for .extend() have the same shape as `manage_nvim_cmp`:
+      -- https://github.com/VonHeikemen/lsp-zero.nvim/blob/v2.x/doc/md/api-reference.md#manage_nvim_cmp
+
+      require('lsp-zero.cmp').extend()
+
+      -- And you can configure cmp even more, if you want to.
+      local cmp = require('cmp')
+      local cmp_action = require('lsp-zero.cmp').action()
+
+      cmp.setup({
+        mapping = {
+            ['<CR>'] = cmp.mapping.confirm({select = false}),
+            ['<C-Space>'] = cmp.mapping.complete(),
+            ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+            ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+            ['<Tab>'] = vim.NIL,
+            ['<S-Tab>'] = vim.NIL,
+        },
+        sources = {
+            { name = "nvim_lsp" },
+            { name = "nvim_lua" },
+            { name = 'nvim_lsp_signature_help' },
+            { name = "luasnip" },
+            { name = "buffer" },
+            { name = "path" },
+            { name = "crates" },
+            -- { name = "copilot" },
+        },
+        window = {
+            completion = cmp.config.window.bordered(),
+            documentation = cmp.config.window.bordered(),
+        },
+        sorting = {
+          priority_weight = 2,
+          comparators = {
+            -- require("copilot_cmp.comparators").prioritize,
+
+            -- Below is the default comparitor list and order for nvim-cmp
+            cmp.config.compare.offset,
+            -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
+            cmp.config.compare.exact,
+            cmp.config.compare.score,
+            cmp.config.compare.recently_used,
+            cmp.config.compare.locality,
+            cmp.config.compare.kind,
+            cmp.config.compare.sort_text,
+            cmp.config.compare.length,
+            cmp.config.compare.order,
+          },
+        },
+    })
+        end
+  },
+
+  -- LSP
+  {
+    'neovim/nvim-lspconfig',
+    cmd = 'LspInfo',
+    event = {'BufReadPre', 'BufNewFile'},
+    dependencies = {
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'williamboman/mason-lspconfig.nvim'},
+      {
+        'williamboman/mason.nvim',
+        build = function()
+          pcall(vim.cmd, 'MasonUpdate')
+        end,
+      },
+    },
+    config = function()
+      local lsp = require('lsp-zero')
+
+      lsp.on_attach(function(client, bufnr)
+        lsp.default_keymaps({buffer = bufnr})
+      end)
+
+      local rust_lsp = lsp.build_options('rust_analyzer', {})
+
+      local lspconfig = require('lspconfig')
+
+      lspconfig.lua_ls.setup(lsp.nvim_lua_ls())
+
+      lspconfig.yamlls.setup({
+          settings = {
+              yaml = {
+                  customTags = {
+                      "!vault",
+                  },
+                  keyOrdering = false
+              }
+          }
+      })
+
+      lsp.setup()
+
+      -- Initialize rust_analyzer with rust-tools
+      require('rust-tools').setup({server = rust_lsp})
+
+    end
+  }
 }
