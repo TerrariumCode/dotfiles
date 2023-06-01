97d979e5e77b813d4eee8fcbd0e6634122f8599c
cmac4603 <cmac4603@users.noreply.github.com>
2023-06-01 13:25:54 +0100

    copilot not lazy

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,6 +1,7 @@
 return {
     {
         "github/copilot.vim",
+        lazy = false,
         init = function ()
             vim.g.copilot_no_tab_map = true
             vim.g.copilot_assume_mapped = true
