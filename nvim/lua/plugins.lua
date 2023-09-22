cd581eb150e26d8813ca591767eb940f2a00010f
cmac4603 <cmac4603@users.noreply.github.com>
2023-09-22 11:19:13 +0100

    remove lualine for feline, install symbol-usage, disable flash search

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,6 +1,10 @@
 return {
 	"nvim-lua/plenary.nvim", -- Useful lua functions used by lots of plugins
 	{
+		"freddiehaddad/feline.nvim",
+		config = true,
+	},
+	{
 		"folke/todo-comments.nvim",
 		config = true,
 	},
---
+++ nvim/lua/plugins/symbol-usage.lua
@@ -0,0 +1,11 @@
+return {
+	"Wansmer/symbol-usage.nvim",
+	event = "BufReadPre", -- need run before LspAttach if you use nvim 0.9. On 0.10 use 'LspAttach'
+	config = function()
+		local opts = {
+			---@type 'above'|'end_of_line'|'textwidth'
+			vt_position = "end_of_line",
+		}
+		require("symbol-usage").setup(opts)
+	end,
+}
