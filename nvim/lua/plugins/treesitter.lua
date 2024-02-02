6b7dc6eb958703c0f0d80875563ea76c269052da
cmac4603 <cmac4603@users.noreply.github.com>
2024-02-02 11:31:23 +0000

    treesitter text objects select

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,19 +2,30 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		opts = {
-			{
-				ensure_installed = "all", -- one of "all" or a list of languages
-				ignore_install = { "" }, -- List of parsers to ignore installing
-				sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
+			ensure_installed = "all", -- one of "all" or a list of languages
+			ignore_install = { "" }, -- List of parsers to ignore installing
+			sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
 
-				highlight = {
-					enable = true, -- false will disable the whole extension
-					disable = { "css", "rust" }, -- list of language that will be disabled
-				},
-				autopairs = {
+			highlight = {
+				enable = true, -- false will disable the whole extension
+				disable = { "css", "rust" }, -- list of language that will be disabled
+			},
+			autopairs = {
+				enable = true,
+			},
+			indent = { enable = true, disable = { "css" } },
+			textobjects = {
+				select = {
 					enable = true,
+					lookahead = true,
+					keymaps = {
+						-- You can use the capture groups defined in textobjects.scm
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						["ic"] = "@class.inner",
+					},
 				},
-				indent = { enable = true, disable = { "css" } },
 			},
 		},
 	},
