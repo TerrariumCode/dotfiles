ef77962a9e5a6ba54246ceb95eb5645a72e63125
cmac4603 <cmac4603@users.noreply.github.com>
2024-03-28 13:29:43 +0000

    remove kb shortcuts

--- fish/config-play.fish
+++ fish/config-play.fish
@@ -26,14 +26,3 @@ export LLDB_EXEC_PATH='/bin/lldb-vscode'
 
 # morhetz/gruvbox
 export FZF_DEFAULT_OPTS='--color=bg+:#3c3836,bg:#32302f,spinner:#fb4934,hl:#928374,fg:#ebdbb2,header:#928374,info:#8ec07c,pointer:#fb4934,marker:#fb4934,fg+:#ebdbb2,prompt:#fb4934,hl+:#fb4934'
-
-abbr -a -- l exa --long --header --git
-
-# git things
-abbr -a -- ga git add
-abbr -a -- gau git add -u
-abbr -a -- gaa git add -A
-abbr -a -- gc git commit
-abbr -a -- gcmsg git commit -m
-abbr -a -- gp git push
-abbr -a -- gst git status
