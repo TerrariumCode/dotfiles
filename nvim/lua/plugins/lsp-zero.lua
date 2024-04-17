fd7bc8a21711839e5b726252c5a21edd3deabefe
cmac4603 <cmac4603@users.noreply.github.com>
2024-04-17 17:46:08 +0100

    update rust nvim addon

--- nvim/lua/plugins/lsp-zero.lua
+++ nvim/lua/plugins/lsp-zero.lua
@@ -65,8 +65,34 @@ return {
 
     {
         "mrcjkb/rustaceanvim",
-        version = "^3", -- Recommended
+        version = "^4", -- Recommended
+        lazy = false,
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "mfussenegger/nvim-dap",
+            {
+                "lvimuser/lsp-inlayhints.nvim",
+                opts = {}
+            },
+        },
         ft = { "rust" },
+        config = function()
+            vim.g.rustaceanvim = {
+                inlay_hints = {
+                    highlight = "NonText",
+                },
+                tools = {
+                    hover_actions = {
+                        auto_focus = true,
+                    },
+                },
+                server = {
+                    on_attach = function(client, bufnr)
+                        require("lsp-inlayhints").on_attach(client, bufnr)
+                    end
+                }
+            }
+        end
     },
     {
         "saecki/crates.nvim",
