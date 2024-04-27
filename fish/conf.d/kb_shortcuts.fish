d351bb37fffaa05cc828b9d3d935cade5508b3c3
cmac4603 <cmac4603@users.noreply.github.com>
2024-04-27 20:41:42 +0100

    tmux abbr

--- fish/conf.d/kb_shortcuts.fish
+++ fish/conf.d/kb_shortcuts.fish
@@ -48,6 +48,7 @@ abbr -a -- clear-dns "sudo killall -HUP mDNSResponder"
 abbr -a -- l exa --all --long --header --git
 abbr -a -- ngrok-hearst ngrok http --domain=cmac4603-hearst.ngrok.io 8081
 abbr -a -- nv nvim
+abbr -a -- t tmux
 
 # vault (hashicorp) things
 abbr -a -- vlogin vault login -method=github token=$GITHUB_TOKEN
