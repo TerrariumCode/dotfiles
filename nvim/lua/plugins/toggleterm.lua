332074924ed1c8aa2d074f015105336944b0fb88
cmac4603 <cmac4603@users.noreply.github.com>
2024-04-29 11:43:47 +0100

    toggleterm set to vertical on right...nice

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,35 +1,38 @@
 return {
-	{
-		"akinsho/toggleterm.nvim",
-		opts = {
-			size = 20,
-			open_mapping = [[<c-\>]],
-			hide_numbers = true,
-			shade_terminals = true,
-			shading_factor = 2,
-			start_in_insert = true,
-			insert_mappings = true,
-			persist_size = true,
-			persist_mode = true,
-			direction = "float",
-			close_on_exit = true,
-			shell = vim.o.shell,
-			float_opts = {
-				border = "curved",
-			},
-		},
-	},
-	{
-		"https://git.sr.ht/~havi/telescope-toggleterm.nvim",
-		event = "TermOpen",
-		requires = {
-			"akinsho/nvim-toggleterm.lua",
-			"nvim-telescope/telescope.nvim",
-			"nvim-lua/popup.nvim",
-			"nvim-lua/plenary.nvim",
-		},
-		config = function()
-			require("telescope").load_extension("toggleterm")
-		end,
-	},
+    {
+        "akinsho/toggleterm.nvim",
+        opts = {
+            size = function(term)
+                if term.direction == "horizontal" then
+                    return 15
+                elseif term.direction == "vertical" then
+                    return vim.o.columns * 0.35
+                end
+            end,
+            open_mapping = [[<c-\>]],
+            hide_numbers = true,
+            shade_terminals = true,
+            shading_factor = 2,
+            start_in_insert = true,
+            insert_mappings = true,
+            persist_size = true,
+            persist_mode = true,
+            direction = "vertical",
+            close_on_exit = true,
+            shell = vim.o.shell,
+        },
+    },
+    {
+        "https://git.sr.ht/~havi/telescope-toggleterm.nvim",
+        event = "TermOpen",
+        requires = {
+            "akinsho/nvim-toggleterm.lua",
+            "nvim-telescope/telescope.nvim",
+            "nvim-lua/popup.nvim",
+            "nvim-lua/plenary.nvim",
+        },
+        config = function()
+            require("telescope").load_extension("toggleterm")
+        end,
+    },
 }
