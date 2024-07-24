c04a060fe393103f9c7661da92ca3373608a61f1
cmac4603 <cmac4603@users.noreply.github.com>
2024-07-24 09:51:22 +0100

    lualine config formatting

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,18 +1,18 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = {
-		options = {
-			section_separators = { "", "" },
-			component_separators = { "", "" },
-		},
-		sections = {
-			lualine_a = { "mode" },
-			lualine_b = { "branch", "diff", "diagnostics" },
-			lualine_c = { "filename", "location" },
-			lualine_x = { "encoding", "fileformat", "filetype" },
-			lualine_y = { "progress" },
-			lualine_z = {},
-		},
-	},
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = {
+        options = {
+            section_separators = { "", "" },
+            component_separators = { "", "" },
+        },
+        sections = {
+            lualine_a = { "mode" },
+            lualine_b = { "branch", "diff", "diagnostics" },
+            lualine_c = { "filename", "location" },
+            lualine_x = { "encoding", "fileformat", "filetype" },
+            lualine_y = { "progress" },
+            lualine_z = {},
+        },
+    },
 }
