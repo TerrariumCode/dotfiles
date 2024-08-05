a67ba8cf36e44f8341ff309a6aab4181886c0d59
cmac4603 <cmac4603@users.noreply.github.com>
2024-08-05 10:32:15 +0100

    rm pos dashboard for alpha again, simpler

---
+++ nvim/lua/plugins/alpha.lua
@@ -0,0 +1,45 @@
+return {
+    "goolord/alpha-nvim",
+    config = function()
+        local alpha = require("alpha")
+        local dashboard = require("alpha.themes.dashboard")
+
+        dashboard.section.header.val = {
+            " ██████╗███╗   ███╗ █████╗  ██████╗██╗  ██╗ ██████╗  ██████╗ ██████╗ ",
+            "██╔════╝████╗ ████║██╔══██╗██╔════╝██║  ██║██╔════╝ ██╔═████╗╚════██╗",
+            "██║     ██╔████╔██║███████║██║     ███████║███████╗ ██║██╔██║ █████╔╝",
+            "██║     ██║╚██╔╝██║██╔══██║██║     ╚════██║██╔═══██╗████╔╝██║ ╚═══██╗",
+            "╚██████╗██║ ╚═╝ ██║██║  ██║╚██████╗     ██║╚██████╔╝╚██████╔╝██████╔╝",
+            " ╚═════╝╚═╝     ╚═╝╚═╝  ╚═╝ ╚═════╝     ╚═╝ ╚═════╝  ╚═════╝ ╚═════╝ ",
+        }
+
+        dashboard.section.buttons.val = {
+            dashboard.button(
+                "f",
+                " " .. " Find file",
+                ":Telescope find_files find_command=rg,--ignore,--hidden,--files<CR>"
+            ),
+            dashboard.button("e", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+            dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+            dashboard.button(
+                "t",
+                " " .. " Find text",
+                ":Telescope live_grep find_command=rg,--ignore,--hidden,--files<CR>"
+            ),
+            dashboard.button("q", " " .. " Quit", ":qa<CR>"),
+        }
+
+        local function footer()
+            return "cmac4603"
+        end
+
+        dashboard.section.footer.val = footer()
+
+        dashboard.section.footer.opts.hl = "Type"
+        dashboard.section.header.opts.hl = "Include"
+        dashboard.section.buttons.opts.hl = "Keyword"
+
+        dashboard.opts.opts.noautocmd = true
+        alpha.setup(dashboard.opts)
+    end,
+}
