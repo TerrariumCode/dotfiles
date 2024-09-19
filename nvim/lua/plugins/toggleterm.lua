4be7f1acb33f458d3563420adfd3f328505d87c0
cmac4603 <cmac4603@users.noreply.github.com>
2024-09-19 13:03:13 +0100

    rm bad pull

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -33,10 +33,6 @@ return {
         },
         config = function()
             require("telescope").load_extension("toggleterm")
-            local trim_spaces = true
-            vim.keymap.set("v", "<space>s", function()
-                require("toggleterm").send_lines_to_terminal("single_line", trim_spaces, { args = vim.v.count })
-            end)
         end,
     },
 }
