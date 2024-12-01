e9f06ce0c39e7504d6945075f0accf1e546a88c8
cmac4603 <cmac4603@users.noreply.github.com>
2024-12-01 21:27:11 +0000

    source asdf file

--- fish/config-play.fish
+++ fish/config-play.fish
@@ -31,3 +31,6 @@ export LLDB_EXEC_PATH='/bin/lldb-vscode'
 export FZF_DEFAULT_OPTS='--color=bg+:#3c3836,bg:#32302f,spinner:#fb4934,hl:#928374,fg:#ebdbb2,header:#928374,info:#8ec07c,pointer:#fb4934,marker:#fb4934,fg+:#ebdbb2,prompt:#fb4934,hl+:#fb4934'
 
 alias pbcopy='xclip -selection clipboard'
+
+# asdf for elixir (can be used for others too)
+source ~/.asdf/asdf.fish
