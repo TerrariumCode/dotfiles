d08b50921b3c18565a28b5d70fe79de5b95656f7
cmac4603 <cmac4603@users.noreply.github.com>
2025-01-14 11:55:59 +0000

    re-add zen.nvim plugin

---
+++ nvim/lua/plugins/zen.lua
@@ -0,0 +1,14 @@
+return {
+    "folke/zen-mode.nvim",
+    opts = {
+        plugins = {
+            alacritty = {
+                enabled = false,
+                font = "14",
+            },
+            tmux = {
+                enabled = true,
+            },
+        },
+    },
+}
