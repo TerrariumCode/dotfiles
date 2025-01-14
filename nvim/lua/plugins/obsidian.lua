78f120df1f0c3d5be07f6639d426660e6908201e
cmac4603 <cmac4603@users.noreply.github.com>
2025-01-14 12:02:55 +0000

    remove  ft from obsidian plugin to stop it starting up on every markdown file

--- nvim/lua/plugins/obsidian.lua
+++ nvim/lua/plugins/obsidian.lua
@@ -2,7 +2,6 @@ return {
     "epwalsh/obsidian.nvim",
     version = "*",
     lazy = true,
-    ft = "markdown",
     event = { "BufReadPre " .. vim.fn.expand "~" .. "/Documents/play_pop_obsidian/**.md" },
     dependencies = {
         "nvim-lua/plenary.nvim",
