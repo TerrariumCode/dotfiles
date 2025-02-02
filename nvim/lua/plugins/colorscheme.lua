bc6815e9928d257eae3bc563f83cba84d691a3d6
cmac4603 <cmac4603@users.noreply.github.com>
2025-02-02 15:42:55 +0000

    feat(colorscheme): switch from zephyr-nvim to morta.nvim
    Replaced the colorscheme plugin from "glepnir/zephyr-nvim" to "philosofonusus/morta.nvim". Updated the configuration to use the new colorscheme.

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,24 +1,9 @@
 return {
-    "glepnir/zephyr-nvim",
-    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
+    "philosofonusus/morta.nvim",
+    name = 'morta',
+    priority = 1000,
     opts = {},
     config = function()
-        vim.cmd.colorscheme 'zephyr'
-        vim.g.zephyr_disable_toggle_style = 1
-        vim.g.zephyr_cursor = "lCursor"
-        vim.g.zephyr_italic = 1
-        vim.g.zephyr_italic_comment = 1
-        vim.g.zephyr_italic_keyword = 1
-        vim.g.zephyr_italic_function = 1
-        vim.g.zephyr_italic_variable = 1
-        vim.g.zephyr_italic_string = 1
-        vim.g.zephyr_italic_number = 1
-        vim.g.zephyr_italic_boolean = 1
-        vim.g.zephyr_italic_constant = 1
-        vim.g.zephyr_italic_class = 1
-        vim.g.zephyr_italic_operator = 1
-        vim.g.zephyr_italic_exception = 1
-        vim.g.zephyr_italic_preproc = 1
-    end
+        vim.cmd.colorscheme 'morta'
+    end,
 }
