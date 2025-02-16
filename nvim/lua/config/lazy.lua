4475c7488726824b952029be04d1a5b3d7e6cdb1
cmac4603 <cmac4603@users.noreply.github.com>
2025-02-16 17:21:25 +0000

    refactor(config): consolidate and reorganize Neovim configuration
    - Moved lazy.nvim bootstrap and configuration to `lua/config/lazy.lua`.
    - Deleted `autocommands.lua`, `keymaps.lua`, and `options.lua` by consolidating their contents into `config/lazy.lua`.

---
+++ nvim/lua/config/lazy.lua
@@ -0,0 +1,276 @@
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+    -- import your plugins
+    { import = "plugins" },
+  },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
+
+vim.g.mapleader = " "
+-- skip backwards compatibility routines and speed up loading
+vim.g.skip_ts_context_commentstring_module = true
+
+vim.g.markdown_fenced_languages = {
+    "ts=typescript",
+}
+
+vim.fn.sign_define(
+    'DiagnosticSignError',
+    { texthl = 'DiagnosticSignError', text = '', numhl = 'DiagnosticSignError' }
+)
+
+vim.fn.sign_define(
+    'DiagnosticSignWarn',
+    { texthl = 'DiagnosticSignWarn', text = '', numhl = 'DiagnosticSignWarn' }
+)
+
+vim.fn.sign_define(
+    'DiagnosticSignHint',
+    { texthl = 'DiagnosticSignHint', text = '', numhl = 'DiagnosticSignHint' }
+)
+
+vim.fn.sign_define(
+    'DiagnosticSignInfo',
+    { texthl = 'DiagnosticSignInfo', text = '', numhl = 'DiagnosticSignInfo' }
+)
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+    pattern = { "qf", "help", "man", "lspinfo", "spectre_panel" },
+    callback = function()
+        vim.cmd([[
+      nnoremap <silent> <buffer> q :close<CR>
+      set nobuflisted
+    ]])
+    end,
+})
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+    pattern = { "gitcommit", "markdown" },
+    callback = function()
+        vim.opt_local.spell = true
+    end,
+})
+
+vim.api.nvim_create_autocmd({ "VimResized" }, {
+    callback = function()
+        vim.cmd("tabdo wincmd =")
+    end,
+})
+
+vim.api.nvim_create_autocmd({ "CmdWinEnter" }, {
+    callback = function()
+        vim.cmd("quit")
+    end,
+})
+
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+    callback = function()
+        vim.highlight.on_yank({ higroup = "Visual", timeout = 200 })
+    end,
+})
+
+vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+    pattern = { "*.java" },
+    callback = function()
+        vim.lsp.codelens.refresh()
+    end,
+})
+
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+    callback = function()
+        vim.cmd("StripTrailingWhitespace")
+    end,
+})
+
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+    pattern = { "*.tf", "*.tfvars" },
+    callback = function()
+        vim.lsp.buf.format()
+    end,
+})
+
+vim.api.nvim_create_autocmd({ 'BufNewFile', 'BufRead' }, {
+    pattern = 'deployment.yaml',
+    callback = function()
+        vim.opt_local.filetype = 'helm'
+    end
+})
+
+-- KEYMAPS
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+-- Silent keymap option
+local opts = { silent = true }
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+
+keymap("v", "<leader>y", '"+y', { noremap = true })
+
+keymap("n", "<leader>c", ":bp<bar>sp<bar>bn<bar>bd<CR>", { noremap = true, silent = true })
+keymap("n", "<C-S>s", ":vert sb", { noremap = true, silent = true })
+
+keymap("n", "<C-,>", ":30winc <<CR>", opts)
+keymap("n", "<C-.>", ":30winc ><CR>", opts)
+
+keymap("t", "<C-\\><C-\\>", "<C-\\><C-n>", { noremap = true })
+
+keymap("n", "<C-d>", "<C-d>zz", opts)
+keymap("n", "<C-u>", "<C-u>zz", opts)
+-- save file
+keymap("n", "<C-s>", ":w<CR>", opts)
+keymap("i", "<C-s>", "<ESC>:w<CR>", opts)
+
+keymap("n", "<leader>fml", "<cmd>CellularAutomaton make_it_rain<CR>", opts)
+
+keymap("", "<Up>", "<Nop>", { noremap = true, silent = true })
+keymap("", "<Down>", "<Nop>", { noremap = true, silent = true })
+keymap("", "<Left>", "<Nop>", { noremap = true, silent = true })
+keymap("", "<Right>", "<Nop>", { noremap = true, silent = true })
+
+-- jump back and forth between last two buffers
+keymap("n", "<leader>j", "<C-^>", opts)
+
+-- neotree toggle
+keymap("n", "<leader>n", ":Neotree toggle<CR>", opts)
+
+-- enable treesitter highlighting
+keymap("n", "<leader>i", ":TSBufEnable highlight<CR>", opts)
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- Normal --
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Clear highlights
+keymap("n", "<leader>h", "<cmd>nohlsearch<CR>", opts)
+
+-- Better paste
+keymap("v", "p", '"_dP', opts)
+
+-- Insert --
+keymap("i", "jk", "<ESC>", opts)           -- Press jk fast to enter
+keymap("v", "J", ":m '>+1<CR>gv=gv", opts) -- Shift visual selected line down
+keymap("v", "K", ":m '<-2<CR>gv=gv", opts) -- Shift visual selected line up
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Plugins --
+
+-- Comment
+keymap("n", "<leader>/", "<cmd>lua require('Comment.api').toggle.linewise.current()<CR>", opts)
+keymap("x", "<leader>/", '<ESC><CMD>lua require("Comment.api").toggle.linewise(vim.fn.visualmode())<CR>', opts)
+
+-- DAP
+keymap("n", "<leader>db", "<cmd>lua require'dap'.toggle_breakpoint()<cr>", opts)
+keymap("n", "<leader>dc", "<cmd>lua require'dap'.continue()<cr>", opts)
+keymap("n", "<leader>di", "<cmd>lua require'dap'.step_into()<cr>", opts)
+keymap("n", "<leader>do", "<cmd>lua require'dap'.step_over()<cr>", opts)
+keymap("n", "<leader>dO", "<cmd>lua require'dap'.step_out()<cr>", opts)
+keymap("n", "<leader>dr", "<cmd>lua require'dap'.repl.toggle()<cr>", opts)
+keymap("n", "<leader>dl", "<cmd>lua require'dap'.run_last()<cr>", opts)
+keymap("n", "<leader>du", "<cmd>lua require'dapui'.toggle()<cr>", opts)
+keymap("n", "<leader>dt", "<cmd>lua require'dap'.terminate()<cr>", opts)
+-- DAP Python
+keymap("n", "<leader>dn", "<cmd>lua require('dap-python').test_method()<cr>", opts)
+keymap("n", "<leader>df", "<cmd>lua require('dap-python').test_class()<cr>", opts)
+keymap("v", "<leader>ds", "<esc><cmd>lua require('dap-python').debug_selection()<cr>", opts)
+
+keymap("n", "<leader>csv", "<cmd>lua require('nvim-preview-csv').preview()<cr>", opts)
+
+-- OPTIONS
+vim.opt.title = false
+vim.opt.relativenumber = true
+vim.opt.colorcolumn = "80,120"
+vim.opt.clipboard = ""
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.backup = false                          -- creates a backup file
+vim.opt.cmdheight = 1                           -- more space in the neovim command line for displaying messages
+vim.opt.completeopt = { "menuone", "noselect" } -- mostly just for cmp
+vim.opt.conceallevel = 0                        -- so that `` is visible in markdown files
+vim.opt.fileencoding = "utf-8"                  -- the encoding written to a file
+vim.opt.hlsearch = true                         -- highlight all matches on previous search pattern
+vim.opt.ignorecase = true                       -- ignore case in search patterns
+vim.opt.mouse = "a"                             -- allow the mouse to be used in neovim
+vim.opt.pumheight = 10                          -- pop up menu height
+vim.opt.showmode = false                        -- we don't need to see things like -- INSERT -- anymore
+vim.opt.showtabline = 0                         -- always show tabs
+vim.opt.smartcase = true                        -- smart case
+vim.opt.smartindent = true                      -- make indenting smarter again
+vim.opt.splitbelow = true                       -- force all horizontal splits to go below current window
+vim.opt.splitright = true                       -- force all vertical splits to go to the right of current window
+vim.opt.swapfile = false                        -- creates a swapfile
+vim.opt.termguicolors = true                    -- set term gui colors (most terminals support this)
+vim.o.background = "dark"
+vim.opt.timeoutlen = 500                        -- time to wait for a mapped sequence to complete (in milliseconds)
+vim.opt.undofile = true                         -- enable persistent undo
+vim.opt.updatetime = 200                        -- faster completion (4000ms default)
+vim.opt.writebackup = false                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+vim.opt.expandtab = true                        -- convert tabs to spaces
+vim.opt.cursorline = true                       -- highlight the current line
+vim.opt.number = true                           -- set numbered lines
+vim.opt.laststatus = 3                          -- only the last window will always have a status line
+vim.opt.showcmd = false                         -- hide (partial) command in the last line of the screen (for performance)
+vim.opt.ruler = false                           -- hide the line and column number of the cursor position
+vim.opt.numberwidth = 4                         -- minimal number of columns to use for the line number {default 4}
+vim.opt.signcolumn = "yes"                      -- always show the sign column, otherwise it would shift the text each time
+vim.opt.wrap = false                            -- display lines as one long line
+vim.opt.scrolloff = 8                           -- minimal number of screen lines to keep above and below the cursor
+vim.opt.sidescrolloff = 8                       -- minimal number of screen columns to keep to the left and right of the cursor if wrap is `false`
+vim.opt.guifont = "monospace:h17"               -- the font used in graphical neovim applications
+vim.opt.fillchars.eob = " "                     -- show empty lines at the end of a buffer as ` ` {default `~`}
+vim.opt.shortmess:append("c")                   -- hide all the completion messages, e.g. "-- XXX completion (YYY)", "match 1 of 2", "The only match", "Pattern not found"
+vim.opt.whichwrap:append("<,>,[,],h,l")         -- keys allowed to move to the previous/next line when the beginning/end of line is reached
+vim.opt.iskeyword:append("-")                   -- treats words with `-` as single words
+vim.opt.formatoptions:remove({ "c", "r", "o" }) -- This is a sequence of letters which describes how automatic formatting is to be done
+vim.opt.linebreak = true
+vim.opt.list = true
+vim.opt.listchars = {
+    eol = "↲",
+}
