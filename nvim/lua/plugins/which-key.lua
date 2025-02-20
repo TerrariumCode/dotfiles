3d7c9a5858677f7632a1eacac7322ab9bcc258d0
TerrariumCode <12590206+TerrariumCode@users.noreply.github.com>
2025-02-20 08:57:12 +0000

    feat(which-key): add new keybindings for CodeCompanion actions
    - Added keybindings for explaining code and generating unit tests.
    - Updated existing keybindings to include mode specifications for normal and visual modes.
    - Improved alignment and formatting for better readability.

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -14,26 +14,28 @@ return {
         local wk = require("which-key")
 
         wk.add({
-            { "<leader>a", group = "AI" },
-            { "<leader>aa", "<CMD>CodeCompanionActions<CR>", desc = "Actions" },
-            { "<leader>ac", "<CMD>CodeCompanion /commit<CR>", desc = "Commit Message" },
+            { "<leader>a",  group = "AI" },
+            { "<leader>aa", "<CMD>CodeCompanionActions<CR>",     desc = "Actions",       mode = { "n", "v" } },
+            { "<leader>ac", "<CMD>CodeCompanion /commit<CR>",    desc = "Commit Message" },
+            { "<leader>ae", "<CMD>CodeCompanion /explain<CR>",   desc = "Explain Code",  mode = { "n", "v" } },
             { "<leader>at", "<CMD>CodeCompanionChat Toggle<CR>", desc = "Chat Toggle" },
+            { "<leader>au", "<CMD>CodeCompanion /unittests<CR>", desc = "Generate Unit Tests",   mode = { "n", "v" } },
         })
 
         wk.add({
-            { "<leader>c", group = "CSV" },
+            { "<leader>c",   group = "CSV" },
             { "<leader>csv", "<CMD>CsvViewToggle<CR>", desc = "Toggle CSV View" },
         })
 
         wk.add({
             { "<leader>g",  group = "Git" },
-            { "<leader>gb", "<CMD>Gitsigns blame_line<CR>", desc = "Git Blame Line" },
-            { "<leader>gd", "<CMD>Neogit diff<CR>", desc = "Neogit Diff" },
-            { "<leader>gl", "<CMD>Neogit log<CR>", desc = "Neogit Log" },
-            { "<leader>gn", "<CMD>Neogit<CR>", desc = "Neogit Open" },
+            { "<leader>gb", "<CMD>Gitsigns blame_line<CR>",    desc = "Git Blame Line" },
+            { "<leader>gd", "<CMD>Neogit diff<CR>",            desc = "Neogit Diff" },
+            { "<leader>gl", "<CMD>Neogit log<CR>",             desc = "Neogit Log" },
+            { "<leader>gn", "<CMD>Neogit<CR>",                 desc = "Neogit Open" },
             { "<leader>go", function() Snacks.gitbrowse() end, desc = "Open Remote Git URL" },
-            { "<leader>gp", "<CMD>Neogit push<CR>", desc = "Neogit Push" },
-            { "<leader>gr", "<CMD>Neogit rebase<CR>", desc = "Neogit Rebase" },
+            { "<leader>gp", "<CMD>Neogit push<CR>",            desc = "Neogit Push" },
+            { "<leader>gr", "<CMD>Neogit rebase<CR>",          desc = "Neogit Rebase" },
         })
 
         wk.add({
@@ -60,18 +62,18 @@ return {
 
         wk.add({
             { "<leader>s", "<CMD>ToggleTerm size=20 direction=horizontal name=horizontal<CR>", desc = "Toggle Right Vertical" },
-            { "<leader>S", "<CMD>ToggleTerm size=80 direction=vertical name=vertical<CR>", desc = "Toggle Down Horizontal" },
+            { "<leader>S", "<CMD>ToggleTerm size=80 direction=vertical name=vertical<CR>",     desc = "Toggle Down Horizontal" },
         })
 
         wk.add({
             { "<leader>f",  group = "Telescope" },
-            { "<leader>fb", ":Telescope buffers<CR>", desc = "Find Buffers" },
+            { "<leader>fb", ":Telescope buffers<CR>",                                     desc = "Find Buffers" },
             { "<leader>ff", ":Telescope find_files find_command=rg,--hidden,--files<CR>", desc = "Find Files" },
-            { "<leader>fg", ":Telescope advanced_git_search diff_commit_file<CR>", desc = "Find Commits in File" },
-            { "<leader>fl", ":Telescope advanced_git_search diff_commit_line<CR>", desc = "Find Commits on Line" },
-            { "<leader>fn", ":ScratchList<CR>", desc = "Find Scratch" },
-            { "<leader>fs", ":Telescope toggleterm<CR>", desc = "Find Terminals" },
-            { "<leader>ft", ":Telescope live_grep find_command=rg,--hidden,--files<CR>", desc = "Find Text" },
+            { "<leader>fg", ":Telescope advanced_git_search diff_commit_file<CR>",        desc = "Find Commits in File" },
+            { "<leader>fl", ":Telescope advanced_git_search diff_commit_line<CR>",        desc = "Find Commits on Line" },
+            { "<leader>fn", ":ScratchList<CR>",                                           desc = "Find Scratch" },
+            { "<leader>fs", ":Telescope toggleterm<CR>",                                  desc = "Find Terminals" },
+            { "<leader>ft", ":Telescope live_grep find_command=rg,--hidden,--files<CR>",  desc = "Find Text" },
         })
 
         wk.add({
@@ -83,6 +85,5 @@ return {
             -- { "<leader>z",  group = "Zen Mode" },
             { "<leader>z", "<CMD>ZenMode<CR>", desc = "Toggle" },
         })
-
     end
 }
