1194fe293cb54cc65d88b7c26f701ff6276bfc37
TerrariumCode <12590206+TerrariumCode@users.noreply.github.com>
2025-03-06 09:41:59 +0000

    feat(ai_assistant): add copilot adapter configuration

--- nvim/lua/plugins/ai_assistant.lua
+++ nvim/lua/plugins/ai_assistant.lua
@@ -6,6 +6,17 @@ return {
             "nvim-treesitter/nvim-treesitter",
         },
         opts = {
+            adapters = {
+                copilot = function ()
+                    return require("codecompanion.adapters").extend("copilot", {
+                        -- schema = {
+                        --     model = {
+                        --         default = "claude-3.7-sonnet",
+                        --     },
+                        -- },
+                    })
+                end,
+            },
             strategies = {
                 stream = {
                     adapter = "ollama",
