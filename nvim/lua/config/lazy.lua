88cb2e48396964ff448af9971e6e76e8bb2278d3
cmac4603 <cmac4603@users.noreply.github.com>
2025-03-15 09:49:13 +0000

    feat: add neoscroll.nvim plugin and comment out centering keymaps
    - Added neoscroll.nvim plugin configuration in nvim/lua/plugins/scroll.lua
    - Commented out keymaps for centering after jumping up and down in nvim/lua/config/lazy.lua

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -185,8 +185,10 @@ keymap("n", "<C-.>", ":30winc ><CR>", opts)
 
 keymap("t", "<C-\\><C-\\>", "<C-\\><C-n>", { noremap = true })
 
-keymap("n", "<C-d>", "<C-d>zz", opts)
-keymap("n", "<C-u>", "<C-u>zz", opts)
+-- jump up and down then center
+-- keymap("n", "<C-d>", "<C-d>zz", opts)
+-- keymap("n", "<C-u>", "<C-u>zz", opts)
+
 -- save file
 keymap("n", "<C-s>", ":w<CR>", opts)
 keymap("i", "<C-s>", "<ESC>:w<CR>", opts)
