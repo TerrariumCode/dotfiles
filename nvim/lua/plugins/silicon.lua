1b13d8c1e1af66842d8c48369f57f0546f4a0f63
cmac4603 <cmac4603@users.noreply.github.com>
2025-03-18 23:44:44 +0000

    feat: update configurations and plugin settings
    - Updated `cargo/config.toml` to include `git-fetch-with-cli` setting under `[net]`.
    - Modified `nvim/lua/plugins/silicon.lua` to change build command and add new options: background color, theme, vertical and horizontal padding.
    - Added a new symlink in `playbooks/playbook-play.yaml` for `cargo/config.toml`.

--- nvim/lua/plugins/silicon.lua
+++ nvim/lua/plugins/silicon.lua
@@ -1,7 +1,11 @@
 return {
     "krivahtoo/silicon.nvim",
-    build = "./install.sh build",
+    build = "./install.sh",
     opts = {
+        background = "#87f",
+        theme = "Monokai Extended",
         line_number = true,
+        pad_vert = 80,
+        pad_horiz = 50,
     },
 }
