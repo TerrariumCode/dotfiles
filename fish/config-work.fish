efa80fee4f4f130416d14184e2677a5f63e180e5
TerrariumCode <12590206+TerrariumCode@users.noreply.github.com>
2025-05-13 09:25:29 +0100

    feat: add alias for claude command in work Fish config

--- fish/config-work.fish
+++ fish/config-work.fish
@@ -47,3 +47,4 @@ if [ -f '/Users/cmacrae/Downloads/google-cloud-sdk/path.fish.inc' ]; . '/Users/c
 # Added by OrbStack: command-line tools and integration
 # This won't be added again if you remove it.
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+alias claude="/Users/cmacrae/.claude/local/claude"
