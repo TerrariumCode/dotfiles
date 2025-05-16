7f8f79feb4ebcadb2fcdca26d6e6f93315aefd9a
TerrariumCode <12590206+TerrariumCode@users.noreply.github.com>
2025-05-16 15:21:57 +0100

    feat(fish): add awsecr function for AWS ECR authentication
    Add a new Fish shell function that simplifies the process of logging into both
    public and private AWS ECR repositories using a single command. This improves
    the developer workflow when working with Docker images in ECR.

---
+++ fish/functions/aws_ecr_login.fish
@@ -0,0 +1,7 @@
+# Login to public and private ECR repos
+function awsecr
+    echo "Logging into public ECR..."
+    aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws
+    echo -e "\nLogging into private ECR..."
+    aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 451479211471.dkr.ecr.us-east-1.amazonaws.com
+end
