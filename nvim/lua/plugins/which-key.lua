1d82b6219cd023197d56335d966b8d1e4d499a1e
TerrariumCode <12590206+TerrariumCode@users.noreply.github.com>
2025-06-15 19:37:47 +0100

    fix: formatting

--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -93,7 +93,7 @@ return {
         -- random assortment
         wk.add({
             { "<leader>csv", "<CMD>CsvViewToggle<CR>",                      desc = "Toggle CSV View" },
-            { "<leader>ht",  ":Hardtime toggle<CR>",                            desc = "Hardtime Toggle" },
+            { "<leader>ht",  ":Hardtime toggle<CR>",                        desc = "Hardtime Toggle" },
             { "<leader>l",   function() Snacks.notifier.show_history() end, desc = "Show notification history (logs)" },
             { "<leader>z",   "<CMD>ZenMode<CR>",                            desc = "Zen Toggle" },
         })
