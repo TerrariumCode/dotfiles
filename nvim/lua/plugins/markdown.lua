4bf5113195ca5197aa2d0fd8c5163dc56e64579a
TerrariumCode <12590206+TerrariumCode@users.noreply.github.com>
2025-08-08 14:52:54 +0100

    remove(plugins): remove markview.nvim markdown plugin
    Remove the markview.nvim plugin while keeping markdown-preview.nvim as the
    preferred markdown preview solution.

--- nvim/lua/plugins/markdown.lua
+++ nvim/lua/plugins/markdown.lua
@@ -1,18 +1,5 @@
 return {
     {
-        "OXY2DEV/markview.nvim",
-        lazy = false,
-        dependencies = {
-            "nvim-treesitter/nvim-treesitter",
-            "nvim-tree/nvim-web-devicons"
-        },
-        opts = {
-            preview = {
-                splitview_winopts = { split = "right" },
-            },
-        }
-    },
-    {
         "iamcco/markdown-preview.nvim",
         cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
         build = "cd app && yarn install",
