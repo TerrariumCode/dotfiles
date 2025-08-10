9293e61468cb5a8db326d60cd680e88d1ee74449
TerrariumCode <12590206+TerrariumCode@users.noreply.github.com>
2025-08-10 12:24:11 +0100

    fix(treesitter): add ipkg to ignored parsers
    The ipkg parser is now explicitly ignored during treesitter installation,
    which can prevent potential installation issues with this parser.

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,38 +1,37 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		event = { "BufReadPre", "BufNewFile" },
-		main = "nvim-treesitter.configs",
-		opts = {
-			ensure_installed = "all", -- one of "all" or a list of languages
-			ignore_install = { "" }, -- List of parsers to ignore installing
-			sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-
-			highlight = {
-				enable = true, -- false will disable the whole extension
-				disable = { "css", "rust" }, -- list of language that will be disabled
-			},
-			autopairs = {
-				enable = true,
-			},
-			indent = { enable = true, disable = { "css" } },
-			textobjects = {
-				select = {
-					enable = true,
-					lookahead = true,
-					keymaps = {
-						-- You can use the capture groups defined in textobjects.scm
-						["af"] = "@function.outer",
-						["if"] = "@function.inner",
-						["ac"] = "@class.outer",
-						["ic"] = "@class.inner",
-					},
-				},
-			},
-		},
-	},
-	"nvim-treesitter/nvim-treesitter-context",
-	"nvim-treesitter/nvim-treesitter-textobjects",
-	"JoosepAlviste/nvim-ts-context-commentstring",
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        event = { "BufReadPre", "BufNewFile" },
+        main = "nvim-treesitter.configs",
+        opts = {
+            ensure_installed = "all", -- one of "all" or a list of languages
+            ignore_install = { "ipkg" }, -- List of parsers to ignore installing
+            sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
+            highlight = {
+                enable = true,   -- false will disable the whole extension
+                disable = { "css", "rust" }, -- list of language that will be disabled
+            },
+            autopairs = {
+                enable = true,
+            },
+            indent = { enable = true, disable = { "css" } },
+            textobjects = {
+                select = {
+                    enable = true,
+                    lookahead = true,
+                    keymaps = {
+                        -- You can use the capture groups defined in textobjects.scm
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+                        ["ac"] = "@class.outer",
+                        ["ic"] = "@class.inner",
+                    },
+                },
+            },
+        },
+    },
+    "nvim-treesitter/nvim-treesitter-context",
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    "JoosepAlviste/nvim-ts-context-commentstring",
 }
