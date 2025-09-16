7732bc65788d1a4ded0eb1af42b67a7cffad34eb
TerrariumCode <12590206+TerrariumCode@users.noreply.github.com>
2025-09-16 15:33:44 +0100

    feat(oil): improve file operations workflow
    - Enable autosaving buffers updated by LSP rename operations
    - Skip confirmation prompts for simple edits
    - Disable save prompts when selecting new entries

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -16,6 +16,15 @@ return {
             border = "rounded",
             preview_split = "auto",
         },
+        lsp_file_methods = {
+            -- Set to true to autosave buffers that are updated with LSP willRenameFiles
+            -- Set to "unmodified" to only save unmodified buffers
+            autosave_changes = true,
+        },
+        -- Skip the confirmation popup for simple operations (:help oil.skip_confirm_for_simple_edits)
+        skip_confirm_for_simple_edits = true,
+        -- Selecting a new/moved/renamed file or directory will prompt you to save changes first
+        prompt_save_on_select_new_entry = false,
     },
     dependencies = { "nvim-tree/nvim-web-devicons" },
     lazy = false,
